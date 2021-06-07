595d14522272f005112d088fcae8e39b3cba4c85
Farouk <ghafuz@yahoo.com>
2021-06-07 15:50:55 -0400

    made a change

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-const express = require('express');
-const app = express();
-const port = 8080;
+var express = require('express');
+var app = express();
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', function (req, res) {
+    res.send('<html><body><h1>Hello World</h1></body></html>');
+});
 
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+app.post('/submit-data', function (req, res) {
+    res.send('POST Request');
+});
+
+app.put('/update-data', function (req, res) {
+    res.send('PUT Request');
+});
+
+app.delete('/delete-data', function (req, res) {
+    res.send('DELETE Request');
+});
+
+var server = app.listen(5000, function () {
+    console.log('Node server is running..');
+});
+
